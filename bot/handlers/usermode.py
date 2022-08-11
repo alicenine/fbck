27a46640ba7fabf5f0da15fcbe0d4cd4cb0f207a
vanilla <40017526+plasticsummer@users.noreply.github.com>
2022-08-11 08:03:16 +0600

    Update usermode.py

--- bot/handlers/usermode.py
+++ bot/handlers/usermode.py
@@ -170,7 +170,7 @@ async def supported_media(message: Message):
     builderz.row(InlineKeyboardButton(
         text="🫂 Вступить", url="https://t.me/+Tr4jNXkJUjoxNDhl")
     )
-    #check_member = await bot.get_chat_member(-1001565513038, message.from_user.id)
+    check_member = await bot.get_chat_member(-1001641980120, message.from_user.id)
 
     if message.caption and len(message.caption) > 1000:
         return await message.reply("К сожалению, длина подписи медиафайла превышает допустимый размер. "
